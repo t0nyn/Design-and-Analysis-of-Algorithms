bcd5441794e6d71b4f3b027a8392f27f74905ef6
Antonio Nantes <tony@Antonios-MacBook-Air.local>
2024-08-26 15:59:07 -0400

    feat: arrays generators and bubble sort done

---
+++ SORTING/bubble_sort.py
@@ -0,0 +1,5 @@
+def bubble_sort(vector: list)->list:
+    for i in range(len(vector)-1):
+        for j in range(len(vector)-i-1):
+            if vector[j]>vector[j+1]:
+                vector[j], vector[j+1] = vector[j+1], vector[j]
