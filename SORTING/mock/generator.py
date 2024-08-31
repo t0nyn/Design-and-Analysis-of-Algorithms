395d29cddf3e005d48c75b19dbbe38625466fb4b
Antonio Nantes <tony@Antonios-MacBook-Air.local>
2024-08-31 12:29:35 -0400

    refactor: criando uma classe que agrupe os metodos de gerar listas para maior organizacao

--- SORTING/mock/generator.py
+++ SORTING/mock/generator.py
@@ -1,23 +1,29 @@
 import random
-LOWER_RANDOM_BOUND = 0
-UPPER_RANDOM_BOUND = 10**8
 
-def generate_random_numbers(n: int, rpt: int)->list:
-    return [[random.randint(LOWER_RANDOM_BOUND, UPPER_RANDOM_BOUND) for number in range(n)]for _ in range(rpt)]
+class NumberListGenerator:
+    LOWER_RANDOM_BOUND = 0
+    
+    @staticmethod
+    def generate_random_numbers(n: int, rpt: int)->list:
+        UPPER_RANDOM_BOUND = n**2
+        return [[random.randint(NumberListGenerator.LOWER_RANDOM_BOUND, UPPER_RANDOM_BOUND) for number in range(n)]for _ in range(rpt)]
 
-def generate_sorted_numbers(n: int)->list:
-    return list(range(1, n+1))
+    @staticmethod
+    def generate_sorted_numbers(n: int)->list:
+        return list(range(1, n+1))
 
-def generate_reversed_numbers(n: int)->list:
-    return list(range(n+1, 1, -1))
+    @staticmethod
+    def generate_reversed_numbers(n: int)->list:
+        return list(range(n+1, 1, -1))
 
-def generate_almost_sorted_numbers(n: int)->list:
-    sorted_numbers = generate_sorted_numbers(n)
-    for i in range(n//10):
-        choice1 = sorted_numbers.index(random.choice(sorted_numbers))
-        choice2 = sorted_numbers.index(random.choice(sorted_numbers))
-        sorted_numbers[choice1], sorted_numbers[choice2] = sorted_numbers[choice2], sorted_numbers[choice1]
+    @staticmethod
+    def generate_almost_sorted_numbers(n: int)->list:
+        sorted_numbers = NumberListGenerator.generate_sorted_numbers(n)
+        for i in range(n//20):
+            choice1 = sorted_numbers.index(random.choice(sorted_numbers))
+            choice2 = sorted_numbers.index(random.choice(sorted_numbers))
+            sorted_numbers[choice1], sorted_numbers[choice2] = sorted_numbers[choice2], sorted_numbers[choice1]
 
-    return sorted_numbers
+        return sorted_numbers
 if __name__ == "__main__":
-    print(generate_almost_sorted_numbers(80))
+    print(NumberListGenerator.generate_almost_sorted_numbers(80))
