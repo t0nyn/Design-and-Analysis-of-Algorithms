ca28c00e8298d2be928d128283b9e12c4d09c204
Rafael de Azevedo <rafael.deazevedo01@gmail.com>
2024-08-31 23:22:47 -0400

    chore: cria funcao auxiliar para o quicksort

--- SORTING/quick_sort.py
+++ SORTING/quick_sort.py
@@ -10,10 +10,13 @@ def partition(arr: list, low: int, high: int)->int:
     arr[i+1], arr[high] = arr[high], arr[i+1]
     return i+1
 
-def quick_sort(arr: list, low: int, high: int)->list:
+def recursive_quick_sort(arr: list, low: int, high: int)->list:
     if low < high:
         pivot_index = partition(arr, low, high)
 
-        quick_sort(arr, low, pivot_index-1)
-        quick_sort(arr, pivot_index+1, high)
+        recursive_quick_sort(arr, low, pivot_index-1)
+        recursive_quick_sort(arr, pivot_index+1, high)
     return arr
+
+def quick_sort(arr: list) -> None:
+    recursive_quick_sort(arr, 0, len(arr) - 1)
